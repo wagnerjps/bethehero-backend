54b4c52b677992c08510ce81a5fcbcd3865267f8
Wagner Silva <wagnerjps@gmail.com>
2020-07-22 10:28:25 -0300

    Retornando ao SQLITE

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,19 +2,8 @@ const knex = require("knex");
 const configuration = require("../../knexfile");
 
 const env = process.env.NODE_ENV;
-const config = configuration.development
 
-switch(env){
-    case 'teste':
-        config = configuration.test
-    break;
-    case 'production':
-        config = configuration.production
-    break;
-    default:
-        config = configuration.development
-    break;
-}
+const config = (env === 'test') ? configuration.test : configuration.development;
 
 const connection = knex(config);
 
